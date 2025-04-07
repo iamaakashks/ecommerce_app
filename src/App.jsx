460a713f4b43e44169d00c080b5df715abfe8e7e
Aakash Kumar Suman <121715040+iamaakashks@users.noreply.github.com>
2025-04-08 03:17:49 +0530

    webapp started

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const App = ()=>{
+  return (
+    <div className='w-[80%] min-h-screen bg-red-500 m-auto'>
+      Welcome to the Home page
+    </div>
+  )
+}
+export default App;
