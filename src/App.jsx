9faf65ddf897010533b047a9a9c4477531fc327f
Aakash Kumar Suman <121715040+iamaakashks@users.noreply.github.com>
2025-04-08 06:15:00 +0530

    used react-router-dom to pagify the navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
 import {Navbar} from './components/Navbar'
+import { Route, Routes } from 'react-router-dom';
+import {Home} from './pages/Home.jsx'
+import {Products} from './pages/Products.jsx'
+import {Cart} from './pages/Cart.jsx'
+import {Category} from './pages/Category.jsx'
+
 const App = ()=>{
   return (
-    <div className='w-full h-screen bg-zinc-800 m-auto'>
+    <div className='w-full h-screen bg-zinc-600 m-auto'>
       <Navbar />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/api/products' element={<Products />} />
+        <Route path='/cart' element={<Cart />} />
+        <Route path='/category' element={<Category />} />
+      </Routes>
     </div>
   )
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { FaCartShopping } from "react-icons/fa6";
+import { Link } from "react-router-dom";
 export const Navbar = ()=>{
     const [clicked, setClicked] = useState('')
     const navItems = ['Home', 'Men', 'Women', 'Kids']
@@ -8,7 +9,11 @@ export const Navbar = ()=>{
             <div className="flex items-center h-full px-10 bg-red-500">
                 <ul className="flex w-[70%] gap-28 justify-center">
                     {navItems.map((items)=>{
-                        return <li onClick={()=>setClicked(`${items}`)} className={`${clicked === items ? 'underline underline-offset-4 font-bold' : 'hover:underline-offset-4 hover:underline'} cursor-pointer`} key={items}>{items}</li>
+                        return (
+                            <li key={items}>
+                                <Link to={`/${items.toLowerCase() === 'home' ? '':items.toLowerCase()}`} onClick={()=>setClicked(`${items}`)} className={`${clicked === items ? 'underline underline-offset-4 font-bold' : 'hover:underline-offset-4 hover:underline'} cursor-pointer`}>{items}</Link>
+                            </li>
+                        )
                     })}
                 </ul>
                 <div className="flex items-center w-[30%] justify-end gap-6">
