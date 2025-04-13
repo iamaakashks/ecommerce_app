0d3637d802ad58248e8319b7e42b79c9fd9cf0f9
Aakash Kumar Suman <121715040+iamaakashks@users.noreply.github.com>
2025-04-13 16:37:57 +0530

    signup page setting

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,28 +1,54 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Home } from './Home'
 import { FaHome } from "react-icons/fa";
 import { Link } from 'react-router-dom';
 
 export const Login = ()=>{
+    const [password, setPassword] = useState('');
+    const [confirmPassword, setConfirmPassword] = useState('');
+    const handleCreateButton = (e)=>{
+        e.preventDefault();
+        if(password === confirmPassword && password.length() > 6 && password.trim()!=''){
+            alert('Account Created Successfully')
+        }else{
+            alert("Password must match Re-enter password\nPassword must be of 6 characters\nPassword can't be just spaces");
+            return;
+        }  
+    }
     return (
-        <div className='h-full bg-zinc-900 text-white '>
-            <div className='h-28 flex justify-center items-center'>
-                <Link className='text-3xl' to='/'><FaHome /></Link>
+        <div className='h-full w-full flex flex-col gap-y-8'>
+            <div className='h-28 flex'>
+                <Link className='text-3xl m-auto' to='/'><FaHome /></Link>
             </div>
-            <div className='border-[1px] w-[30%] p-4 m-auto flex flex-col gap-6'>
+            <p className='mx-auto'>Create New Account</p>
+            <form className='border-[1px] border-black w-[30%] p-4 flex flex-col gap-6 mx-auto'>
                 <div className='flex justify-between items-center'>
                     <label for="email">Email</label>
-                    <input required id='email' type='email' placeholder='Email' className='w-3/5 outline-none bg-transparent border-[1px] rounded-md px-2 py-2' />
+                    <input required id='email' type='email' placeholder='Email' className='w-3/5 outline-none bg-transparent border-[1px] border-black rounded-md px-2 py-2' />
                 </div>
                 <div className='flex justify-between items-center'>
                     <label for="password">Password</label>
-                    <input required id='password' type='password' placeholder='Password' className='w-3/5 outline-none bg-transparent border-[1px] rounded-md px-2 py-2' />
+                    <input onChange={
+                        (e)=>{
+                            setPassword(e.target.value);
+                            console.log(password)
+                        }
+                    } value={password} required id='password' type='text' placeholder='Password' className='w-3/5 outline-none bg-transparent border-black border-[1px] rounded-md px-2 py-2' />
+                </div>
+                <div className='flex justify-between items-center'>
+                    <label for="confirmpassword">Re-enter Password</label>
+                    <input onChange={
+                        (e)=>{
+                            setConfirmPassword(e.target.value);
+                            console.log(confirmPassword)
+                        }
+                    } value={confirmPassword} required id='confirmpassword' type='password' placeholder='Password' className='w-3/5 outline-none bg-transparent border-black border-[1px] rounded-md px-2 py-2' />
                 </div>
                 <div className="flex justify-between">
-                    <button className='border-[1px] px-4 py-1.5 rounded-lg bg-green-700 font-semibold'>Login</button>
-                    <button className="cursor-pointer">Forgot Password?</button>
+                    <button onClick={(e)=>handleCreateButton(e)} type='submit' className='border-[1px] px-4 py-1.5 rounded-lg bg-black text-white font-semibold'>Create Account</button>
+                    <p className="font-thin">Already have an Account? <button className='font-bold'>Login</button></p>
                 </div>
-            </div>
+            </form>
         </div>
     )
 }
