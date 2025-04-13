6b6f031ba1b3ec7fe36c2d219be32bc742fe1ed2
Aakash Kumar Suman <121715040+iamaakashks@users.noreply.github.com>
2025-04-14 02:05:05 +0530

    added working contact form

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,13 +1,22 @@
-export default function Conatact(){
+import { useState } from "react"
+
+export default function Contact(){
+    const [isSubmitted, setIsSubmitted] = useState(false);
     return (
         <div className="w-full px-20 py-10 flex">
-            <form className="rounded-md p-10 mx-auto w-[40%] shadow-2xl">
+            <form
+            onClick={()=>setIsSubmitted(true)}
+            action="https://api.web3forms.com/submit"
+            method="POST"
+            className="rounded-md p-10 mx-auto w-[40%] shadow-2xl">
+                <input type="hidden" name='access_key' value="86be57eb-e398-42e9-ac9c-2842a8d2fd0c" />
                 <div className="flex justify-between items-center mb-8">
                     <label className="font-bold" for="email">Email </label>
-                    <input className="w-[80%] bg-transparent outline-none border-[1px] border-black px-3 py-2 rounded-full" id="email" name="e" required placeholder="Email" />
+                    <input className="w-[80%] bg-transparent outline-none border-[1px] border-black px-3 py-2 rounded-full" id="email" name="Email" required placeholder="Email" />
                 </div>
                 <label className="font-bold" for="msg">Send Message</label>
-                <textarea className="mt-2 flex justify-end w-full bg-transparent border-[1px] border-black rounded-md outline-none p-2 resize-none" rows={8} name="msg" id="msg"></textarea>
+                <textarea className="mt-2 flex justify-end w-full bg-transparent border-[1px] border-black rounded-md outline-none p-2 resize-none" rows={8} name="Message" id="msg"></textarea>
+                <input type="hidden" name="redirect" value="https://web3forms.com/success"></input>
                 <button type="submit" className="px-4 py-1.5 bg-black rounded-md text-white mt-8">Send</button>
             </form>
         </div>
